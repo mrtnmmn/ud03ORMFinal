bdac0a8c79d5ab97bbab7ce1eafb0500157f2927
mrtnmmn <mrtn.lmba@gmail.com>
2022-02-21 23:36:39 +0100

    finished c01

--- src/main/java/consultas/C01.java
+++ src/main/java/consultas/C01.java
@@ -10,11 +10,16 @@ public class C01 {
     public DAOVehiculos v = new DAOVehiculos();
 
     public void ListGrupos() {
+        System.out.println("Listando los coches pertenecientes a los grupos E, F y G");
         List<Vehiculos> listVehiculos = v.generateListVehiculoGroups();
+        for (Vehiculos v: listVehiculos) {
+            System.out.println("Matricula:  " + v.getMatricula() + "\nGrupo: " + v.getGrupo());
+        }
     }
 
     public static void main(String[] args) {
-
+        C01 c01 = new C01();
+        c01.ListGrupos();
     }
 
 }
